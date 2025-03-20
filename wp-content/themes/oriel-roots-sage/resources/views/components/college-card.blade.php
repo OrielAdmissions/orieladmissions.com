59c0fe616c458d3e8241f27d6b26b4879725a05d
Sean Khottavong <sean.khottavong@gmail.com>
2025-03-20 18:22:57 -0400

    Update Insights data

--- wp-content/themes/oriel-roots-sage/resources/views/components/college-card.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/components/college-card.blade.php
@@ -3,9 +3,12 @@
   'description' => 'No description available.',
   'acceptance_rate' => 'N/A',
   'applicants' => 'N/A',
-  'lorems' => 'N/A',
   'button_text' => 'Read more',
   'button_link' => '',
+  'applicants_accepted_percentage' => '',
+  'applicants_accepted_total' => '',
+  'applicants_admitted_percentage' => '',
+  'applicants_admitted_total' => ''
 ])
 
 <div
@@ -15,7 +18,7 @@
   class="flex justify-between overflow-hidden rounded-xl bg-white max-lg:flex-col max-lg:flex-wrap-reverse"
 >
   <!-- Left Content -->
-  <div class="basis-full space-y-6 p-6 max-lg:order-1 lg:basis-2/3 lg:p-10">
+  <div class="basis-full space-y-6 p-6 max-lg:order-1 lg:basis-2/3 lg:p-8">
     <h2 class="text-5xl-fluid font-serif max-lg:hidden">
       {{ $college_name }}
     </h2>
@@ -29,7 +32,7 @@
 
   <!-- Right Stats Panel -->
   <div
-    class="bg-chalk/30 flex basis-full flex-col p-6 max-lg:order-0 lg:basis-1/3 lg:p-10"
+    class="bg-chalk/30 flex basis-full flex-col p-6 max-lg:order-0 lg:basis-1/3 lg:p-8"
   >
     <div class="grow">
       <h2 class="text-5xl-fluid mb-6 font-serif lg:hidden">
@@ -38,25 +41,27 @@
       <p class="text-oriel text-6xl-fluid font-serif font-light">
         <span
           class="align-text-bottom"
-          x-text="Math.round(percentage)"
+          x-text="Math.round(percentage*10)/10"
         ></span>
         %
       </p>
-      <p class="font-medium tracking-wide uppercase">
-        Overall Acceptance Rate
-        <br/>
-        Class of 2027
+      <p class="font-medium tracking-wide uppercase text-sm">
+        Overall Acceptance Rate <br class="max-lg:hidden"> Class of 2028
       </p>
     </div>
     <div class="divide-keyline/80 divide-y">
-      <p class="py-4 font-medium uppercase">
-        <span class="text-oriel">{{ $applicants }}</span>
-        Applicants
+      @if($applicants_accepted_percentage && $applicants_accepted_total)
+        <p class="py-4 font-medium uppercase text-sm">
+          <span class="text-oriel">{{ $applicants_accepted_percentage }}</span>
+          applicants accepted out of {{ $applicants_accepted_total }}
+        </p>
+        @endif
+      @if($applicants_admitted_percentage)
+      <p class="py-4 font-medium uppercase text-sm">
+        <span class="text-oriel">{{ $applicants_admitted_percentage }}</span>
+        admitted early decision {{$applicants_admitted_total ? 'out of '. $applicants_admitted_total : '' }}
       </p>
-      <p class="py-4 font-medium uppercase">
-        <span class="text-oriel">{{ $lorems }}</span>
-        Lorems
-      </p>
+        @endif
     </div>
   </div>
 </div>
