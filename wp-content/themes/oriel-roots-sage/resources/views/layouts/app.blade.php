6d1921130d4dc1dcad43bbcbc607f792d324fc25
Sean Khottavong <sean.khottavong@gmail.com>
2025-05-06 08:14:29 -0400

    Add gtm

--- wp-content/themes/oriel-roots-sage/resources/views/layouts/app.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/layouts/app.blade.php
@@ -3,6 +3,27 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Google Tag Manager -->
+
+    <script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start':
+
+            new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+
+          j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+
+      })(window, document, 'script', 'dataLayer', 'GTM-T98TPJPC');</script>
+
+    <!-- End Google Tag Manager -->
     @php(do_action('get_header'))
     @php(wp_head())
 
@@ -10,6 +31,15 @@
   </head>
 
   <body @php(body_class())>
+  <!-- Google Tag Manager (noscript) -->
+
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T98TPJPC"
+
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+
+  <!-- End Google Tag Manager (noscript) -->
     <a class="sr-only focus:not-sr-only" href="#main">
       {{ __('Skip to content', 'sage') }}
     </a>
