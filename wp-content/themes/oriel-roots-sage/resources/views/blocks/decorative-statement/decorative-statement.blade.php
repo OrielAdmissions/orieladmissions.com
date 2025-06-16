3719b402ce05208a35b7f869bc1035a3f400626a
Sean Khottavong <sean.khottavong@gmail.com>
2025-06-16 06:41:49 -0400

    Add decorative statement fields

--- wp-content/themes/oriel-roots-sage/resources/views/blocks/decorative-statement/decorative-statement.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/blocks/decorative-statement/decorative-statement.blade.php
@@ -1,12 +1,14 @@
-<div
-  class="bg-cardinal full-width relative overflow-hidden py-12 text-center md:py-30"
->
-  {!! get_svg('images.oriel-shape-outline', 'absolute top-0 right-0 left-0 mx-auto max-sm:hidden text-white/40') !!}
-  {!! get_svg('images.oriel-shape', 'text-white mx-auto mb-8') !!}
-  <h2 class="text-5xl-fluid fade-in-bottom mx-auto max-w-200 text-white">
-    Oriel Admissions counselors are
-    <span class="text-oriel">involved in every step of this process</span>
-    , spending countless hours supporting their students because applications
-    need to be well thought through and perfectly polished before submissions.
-  </h2>
-</div>
+@php
+  $headline = get_field('headline');
+@endphp
+
+@if ($headline)
+  <div class="bg-cardinal full-width relative overflow-hidden py-12 text-center md:py-30">
+    {!! get_svg('images.oriel-shape-outline', 'absolute top-0 right-0 left-0 mx-auto max-sm:hidden text-white/40') !!}
+    {!! get_svg('images.oriel-shape', 'text-white mx-auto mb-8') !!}
+
+    <h2 class="text-5xl-fluid fade-in-bottom mx-auto max-w-200 text-white">
+      {!! preg_replace('/\|(.+?)\|/', '<span class="text-oriel">$1</span>', e($headline)) !!}
+    </h2>
+  </div>
+@endif
