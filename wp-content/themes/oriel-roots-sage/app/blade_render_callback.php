bc2c79629a6ce0eea756812c2925d96bcf116ff6
Sean Khottavong <sean.khottavong@gmail.com>
2025-06-19 16:36:08 -0400

    Add border radius to image block settings

--- wp-content/themes/oriel-roots-sage/app/blade_render_callback.php
+++ wp-content/themes/oriel-roots-sage/app/blade_render_callback.php
@@ -10,8 +10,36 @@ function blade_render_callback($block, string $content = '', bool $is_preview = 
     $slug = str_replace('acf/', '', $block['name']);
     $block['slug'] = $slug;
 
-    // Render the block template with a wrapper div for scoped JS targeting
-    echo '<div data-acf-block-name="' . esc_attr($block['name']) . '">';
+    // Build the ID attribute
+    $block_id = !empty($block['anchor']) ? $block['anchor'] : 'block-' . $block['id'];
+
+    // Build the class attribute
+    $classes = ['acf-block', 'acf-' . $slug];
+
+    if (!empty($block['align'])) {
+        $classes[] = 'align' . $block['align'];
+    }
+
+    if (!empty($block['className'])) {
+        $classes[] = $block['className'];
+    }
+
+    if (!empty($block['mode'])) {
+        $classes[] = 'is-mode-' . $block['mode'];
+    }
+
+    if (!empty($block['align_text'])) {
+        $classes[] = 'has-text-align-' . $block['align_text'];
+    }
+
+    if (!empty($block['align_content'])) {
+        $classes[] = 'is-vertically-aligned-' . $block['align_content'];
+    }
+
+    $block['classes'] = implode(' ', $classes);
+    $block['block_id'] = $block_id;
+
+    echo '<div id="' . esc_attr($block_id) . '" class="' . esc_attr($block['classes']) . '" data-acf-block-name="' . esc_attr($block['name']) . '">';
     echo \Roots\view('blocks.' . $slug . '.' . $slug, ['block' => $block])->render();
     echo '</div>';
 }
