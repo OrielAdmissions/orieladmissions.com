fa7a80394ba5329dbe329497abf2d36482eaed4f
Sean Khottavong <sean.khottavong@gmail.com>
2025-06-20 13:53:13 -0400

    Update College Services Block

--- wp-content/themes/oriel-roots-sage/resources/views/blocks/college-services/college-services.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/blocks/college-services/college-services.blade.php
@@ -1,28 +1,19 @@
-<section class="space-y-8 py-12 md:py-30">
-  <h2 class="text-6xl-fluid text-center">
-    Our <span class="text-oriel">comprehensive</span> services
-  </h2>
-
-  @if (have_rows('services'))
-    @while (have_rows('services')) @php the_row(); @endphp
-    <x-services-card
-      grade="{{ get_sub_field('grade') }}"
-      title="{{ get_sub_field('title') }}"
-      description="{{ get_sub_field('description') }}"
-      image_id="{{ get_sub_field('image') }}"
-    >
-      <x-slot name="accordion">
-        <x-accordion heading="{{ get_sub_field('accordion_heading') }}">
-          <ul class="grid list-image-(--red-chevron) pl-3 gap-4 md:grid-cols-2">
-            @foreach (get_sub_field('accordion_items') as $item)
-              <li class="text-sm pl-1">
-                <span>{{ $item['text'] }}</span>
-              </li>
-            @endforeach
-          </ul>
-        </x-accordion>
-      </x-slot>
-    </x-services-card>
+@if (have_rows('services'))
+  <div class="space-y-8 py-12 md:py-30">
+    @while (have_rows('services'))
+      @php the_row(); @endphp
+      <x-services-card grade="{{ get_sub_field('grade') }}" title="{{ get_sub_field('title') }}"
+                       description="{{ get_sub_field('description') }}" image_id="{{ get_sub_field('image') }}">
+        @if ($content = get_sub_field('accordion_content'))
+          <x-slot name="accordion">
+            <x-accordion heading="{{ get_sub_field('accordion_heading') }}">
+              <div class="prose max-w-none">
+                {!! $content !!}
+              </div>
+            </x-accordion>
+          </x-slot>
+        @endif
+      </x-services-card>
     @endwhile
-  @endif
-</section>
+  </div>
+@endif
