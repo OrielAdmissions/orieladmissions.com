fda92252299bbeef65d8ee7dca665a1503f0da3e
Sean Khottavong <sean.khottavong@gmail.com>
2025-06-25 09:33:29 -0400

    Fix college insights block

--- wp-content/themes/oriel-roots-sage/resources/views/blocks/college-insights/college-insights.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/blocks/college-insights/college-insights.blade.php
@@ -35,7 +35,7 @@
             ])->toJson() !!} })'
             x-on:keydown.escape.prevent.stop="close($refs.button)"
             x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
-            x-id="['dropdown-button']"
+            x-id="dropdown-button"
             class="relative z-50 w-full"
           >
             <button
