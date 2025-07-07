404bb218de42f22ded2ee6b71bf3b0c4cf0de19b
Sean Khottavong <sean.khottavong@gmail.com>
2025-07-07 07:19:11 -0400

    Update FAQ block

--- wp-content/themes/oriel-roots-sage/resources/views/blocks/faqs/faqs.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/blocks/faqs/faqs.blade.php
@@ -2,20 +2,25 @@
   $faqs = get_field('faqs');
   $title = get_field('title');
   $link = get_field('faq_page_link');
-  $content = get_field('faq_text_content')
+  $content = get_field('faq_text_content');
+  $onFaqPage = is_page('faqs');
 @endphp
 
-<section class="faq-section breakout py-20">
+<section class="faq-section py-20">
   <div class="grid grid-cols-12 place-content-center content-center gap-x-4 gap-y-20">
-    <div class="md:col-span-6 lg:col-span-5 col-span-full lg:col-start-2">
+    <div @class([
+        'md:col-span-4 lg:col-span-3 col-span-full lg:col-start-2' => $onFaqPage,
+        'md:col-span-6 lg:col-span-5 col-span-full lg:col-start-2' => ! $onFaqPage,
+    ])>
       <div class="pin-content">
         @if ($title)
           <h2 class="mb-10 max-w-113 font-serif text-5xl leading-tight font-light">{!! $title !!}</h2>
         @endif
+
         @if($link)
-          <a href="{{ $link }}" target="{!! '_self' !!}"
-             class="btn btn-primary mb-10"> View all FAQs </a>
+          <a href="{{ $link }}" target="{!! '_self' !!}" class="btn btn-primary mb-10"> View all FAQs </a>
         @endif
+
         @if ($content)
           <div class="faqStickyText max-w-42">
             {!! $content !!}
@@ -24,7 +29,10 @@
       </div>
     </div>
 
-    <div class="long-content md:col-span-6 lg:col-span-5 col-span-full">
+    <div @class([
+        'long-content md:col-span-8 lg:col-span-7 col-span-full' => $onFaqPage,
+        'long-content md:col-span-6 lg:col-span-5 col-span-full' => ! $onFaqPage,
+    ])>
       @if ($faqs)
         @foreach ($faqs as $faq)
           <x-accordion :heading="get_the_title($faq)">
